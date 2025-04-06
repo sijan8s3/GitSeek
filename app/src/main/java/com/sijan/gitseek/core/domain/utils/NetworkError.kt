de7219ec237c1b2bfb839345c09b13f4974f0387
jokasta <sijanneupane12@gmail.com>
2025-04-06 15:16:13 -0400

    core module setup

---
+++ app/src/main/java/com/sijan/gitseek/core/domain/utils/NetworkError.kt
@@ -0,0 +1,12 @@
+package com.sijan.gitseek.core.domain.utils
+
+
+enum class NetworkError: Error {
+    REQUEST_TIMEOUT,
+    TOO_MANY_REQUESTS,
+    SERVER_ERROR,
+    SERIALIZATION,
+    UNKNOWN,
+    NO_INTERNET,
+    UNAUTHORIZED
+}
